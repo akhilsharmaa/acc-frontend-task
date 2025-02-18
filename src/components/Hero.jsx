658342889c04e1fded2dc707b46bbec82cfc541e
Akhilesh <akhilsharma3332@gmail.com>
2025-02-18 22:39:43 +0530

    [ui changes]

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,31 +2,29 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <section className="flex flex-row justify-center items-center">
-      <div className="m-10 hero-container border border-gray-200 rounded-lg shadow-xl ">
-        <div className="grid max-w-screen-xl mx-auto  lg:grid-cols-12">
-          <div className="ml-10 mr-auto place-self-center lg:col-span-7">
-            <h2 className="max-w-2xl mb-4 text-2xl  font-extrabold tracking-tight leading-none md:text-6xl text-grey-700">
-              Let`s Learn & Earn
-            </h2>
-            <br/>
-            <p className="mb-6 text-3xl font-light text-black">
-              Get a chance to win up-tp <span className="text-4xl font-bold font- text-blue-500">Rs. 15000</span>
-            </p>  
-            <br/>
+<section className="bg-white py-16 ">
+    <div className=" p-0">
+      
+    <div className="grid w-2/3 bg-primary px-4 py-8  rounded-lg shadow-xl mx-auto lg:gap-8 xl:gap-0  lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7 m-8">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl ">
+              Let`s Learn <br/> & Earn</h1>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl  ">
+              Get a chance to win up-tp <span className="text- font-bold font- text-blue-500">Rs. 15000</span></p>
             <Link  to="refer"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-4 me-4 mb-4">
                Refer Now
             </Link>
-          </div>
-          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <img src="image-101.png" alt="mockup" />
-          </div>
         </div>
-      </div>
+        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img src="image-101.png" alt="mockup" />
+        </div>        
+    </div>
+    </div>
+    <br/>        
 
-    </section>
+</section>
   );
 };
 
-export default Hero;
+export default Hero;  
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,33 +4,18 @@ const Navbar = () => {
             <div className="max-w-screen-xl flex flex-wrap items-center justify-around mx-auto p-4">
                 <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="logo_small.jpeg" className="h-8" alt="Flowbite Logo" />
+                    
+                    <a href="/refer"
+                        className="text-white bg-blue-700 hover:bg-blue-800  focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ">
+                        Refer Now
+                    </a>
+                    
                  </a> 
                 <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
                     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white :bg-gray-800 md::bg-gray-900 :border-gray-700">
                         <li>
                             <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md::text-blue-500 :bg-blue-600 md::bg-transparent" aria-current="page">Home</a>
-                        </li>
-                        <li>
-                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto  md::hover:text-blue-500 :focus:text-white :border-gray-700 :hover:bg-gray-700 md::hover:bg-transparent">Dropdown <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
-                            </svg></button> 
-                            <div id="dropdownNavbar" className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44  :divide-gray-600">
-                                <ul className="py-2 text-sm text-gray-700 :text-gray-400" aria-labelledby="dropdownLargeButton">
-                                    <li>
-                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100  ">Dashboard</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100  ">Settings</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100  ">Earnings</a>
-                                    </li>
-                                </ul>
-                                <div className="py-1">
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100  :text-gray-200 ">Sign out</a>
-                                </div>
-                            </div>
-                        </li>
+                        </li> 
                         <li>
                             <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md::hover:text-blue-500 :hover:bg-gray-700  md::hover:bg-transparent">Services</a>
                         </li>
@@ -40,6 +25,7 @@ const Navbar = () => {
                         <li>
                             <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md::hover:text-blue-500 :hover:bg-gray-700  md::hover:bg-transparent">Contact</a>
                         </li>
+                        
                     </ul>
                 </div>
             </div>
