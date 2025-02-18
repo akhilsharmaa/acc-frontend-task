7cbe82aa087248fe436111ad2f995b7ffb965ffe
Akhilesh <akhilsharma3332@gmail.com>
2025-02-18 12:51:16 +0530

    [footer] initialized succesfuly.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import DialogCard from "./components/DialogCard"; 
+import HowDoReferSection from "./components/HowDoReferSection"; 
+import Footer from "./components/Footer"; 
 
 const App = () => {
   const [showDialog, setShowDialog] = useState(false);
@@ -10,14 +12,20 @@ const App = () => {
   return (
     <div >
       <Navbar />
-      <Hero />
+      <Hero />  
+      <HowDoReferSection/>
+
       <button 
         className="fixed bottom-10 right-10 bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
         onClick={() => setShowDialog(true)}
       >
         Open Dialog
       </button>
+      
+      <Footer/>
+      
       {showDialog && <DialogCard title="Hello!" message="This is a dialog message." onClose={() => setShowDialog(false)} />}
+
     </div>
   );
 }; 
